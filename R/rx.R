660a3ae8e7f8811888fc650ed0a5af4fa2d6b231
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-07-25 18:52:59 -0700

    add examples to all functions

--- R/rx.R
+++ R/rx.R
@@ -19,6 +19,10 @@
 #' @return A logical vector
 #' @seealso [stringr::str_detect()]
 #'
+#' @examples
+#' string <- c(letters[1:4], emoji_name[1:6])
+#' emoji_detect(string)
+#'
 #' @export
 emoji_detect <- function(string, negate = FALSE) {
   str_detect(string, emoji::emoji_rx, negate)
@@ -32,6 +36,12 @@ emoji_detect <- function(string, negate = FALSE) {
 #' @return A character vector
 #'
 #' @seealso [stringr::str_subset()]
+#'
+#' @examples
+#' string <- c(letters[1:4], emoji_name[1:6])
+#'
+#' emoji_subset(string) == emoji_name[1:6]
+#' emoji_subset(string, negate = TRUE)
 #' @export
 emoji_subset <- function(string, negate = FALSE) {
   str_subset(string, emoji::emoji_rx, negate)
@@ -39,6 +49,10 @@ emoji_subset <- function(string, negate = FALSE) {
 
 #' @rdname emoji_subset
 #'
+#' @examples
+#'
+#' emoji_which(string)
+#' emoji_which(string, negate = TRUE)
 #' @export
 emoji_which <- function(string, negate = FALSE) {
   str_which(string, emoji::emoji_rx, negate)
@@ -51,6 +65,12 @@ emoji_which <- function(string, negate = FALSE) {
 #' @param string Input vector
 #' @return An integer vector
 #' @seealso [stringr::str_count()]
+#' @examples
+#' string <- paste(c(letters[1:4], emoji_name[1:6]), collapse = " ")
+#'
+#' emoji_count(string)
+#'
+#' emoji_count(emoji_name[1:6])
 #' @export
 emoji_count <- function(string) str_count(string, emoji::emoji_rx)
 
@@ -63,16 +83,29 @@ emoji_count <- function(string) str_count(string, emoji::emoji_rx)
 #'
 #' @seealso [stringr::str_extract()] and [stringr::str_extract_all()]
 #' @return A character vector
+#'
+#' @examples
+#' chars <- c(letters[1:4], emoji_name[1:6])
+#'
+#' set.seed(1234)
+#'
+#' strings <- lapply(1:10, function(x) paste(sample(chars, x), collapse = ""))
+#'
+#' extracts <- emoji_extract(strings)
 #' @export
 emoji_extract <- function(string) str_extract(string, emoji::emoji_rx)
 
 #' @rdname emoji_extract
+#'
+#' @examples
+#'
+#' all_extracts <- emoji_extract_all(strings)
 #' @export
 emoji_extract_all <- function(string, simplify = FALSE) {
   str_extract_all(string, emoji::emoji_rx, simplify)
 }
 
-#' Extract emojis from a string
+#' Extract matched emojis from a string
 #'
 #' Vectorized over `string`
 #'
@@ -80,10 +113,24 @@ emoji_extract_all <- function(string, simplify = FALSE) {
 #' @return see [stringr::str_match()]
 #'
 #' @seealso [stringr::str_match]
+#'
+#' @examples
+#' chars <- c(letters[1:4], emoji_name[1:6])
+#'
+#' set.seed(1234)
+#'
+#' strings <- lapply(1:10, function(x) paste(sample(chars, x), collapse = ""))
+#'
+#' extracts <- emoji_match(strings)
+#'
 #' @export
 emoji_match <- function(string) str_match(string, emoji::emoji_rx)
 
 #' @rdname emoji_match
+#'
+#' @examples
+#'
+#' extracts <- emoji_match_all(strings)
 #' @export
 emoji_match_all <- function(string) str_match_all(string, emoji::emoji_rx)
 
@@ -97,12 +144,21 @@ emoji_match_all <- function(string) str_match_all(string, emoji::emoji_rx)
 #'   for details
 #'
 #' @return A character vector
+#'
+#' @examples
+#' emoji_replace(emoji_name[1], "_emoji_")
 #' @export
 emoji_replace <- function(string, replacement) {
   str_replace(string, emoji::emoji_rx, replacement)
 }
 
 #' @rdname emoji_replace
+#'
+#' @examples
+#'
+#' string <- paste(c(letters[1:4], emoji_name[1:6]), collapse = " ")
+#'
+#' emoji_replace_all(emoji_name[1:6], "_emoji_")
 #' @export
 emoji_replace_all <- function(string, replacement) {
   str_replace_all(string, emoji::emoji_rx, replacement)
@@ -115,9 +171,18 @@ emoji_replace_all <- function(string, replacement) {
 #' @param string Input vector
 #' @return For `emoji_locate` an integer matrix, for `emoji_locate_all` a list
 #'   of integer matrices
+#'
+#' @examples
+#' string <- paste(c(letters[1:4], emoji_name[1:6]), collapse = " ")
+#'
+#' emoji_locate(string)
 #' @export
 emoji_locate <- function(string) str_locate(string, emoji::emoji_rx)
 
 #' @rdname emoji_locate
+#'
+#' @examples
+#'
+#' emoji_locate_all(string)
 #' @export
 emoji_locate_all <- function(string) str_locate_all(string, emoji::emoji_rx)
