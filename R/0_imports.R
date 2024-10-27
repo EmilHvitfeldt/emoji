51ad87174ea05d7d8cead5204e3369d0bd7ff4b7
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-10-27 13:55:41 -0700

    do upkeep tasks

--- R/0_imports.R
+++
@@ -1,9 +0,0 @@
-#' @importFrom stringr str_detect
-#' @importFrom stringr str_subset
-#' @importFrom stringr str_which
-#' @importFrom stringr str_count
-#' @importFrom stringr str_extract str_extract_all
-#' @importFrom stringr str_match str_match_all
-#' @importFrom stringr str_replace str_replace_all
-#' @importFrom stringr str_locate str_locate_all
-NULL
---
+++ R/emoji-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stringr str_count
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_extract
+#' @importFrom stringr str_extract_all
+#' @importFrom stringr str_locate
+#' @importFrom stringr str_locate_all
+#' @importFrom stringr str_match
+#' @importFrom stringr str_match_all
+#' @importFrom stringr str_replace
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_subset
+#' @importFrom stringr str_which
+## usethis namespace: end
+NULL
